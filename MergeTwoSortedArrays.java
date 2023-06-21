9c63a67eaafe6355274f6321b8ec8b0008bd9c26
snehadudyala93 <97564077+snehadudyala93@users.noreply.github.com>
2023-06-21 18:24:32 -0500

    comments added

--- MergeTwoSortedArrays.java
+++ MergeTwoSortedArrays.java
@@ -1,30 +1,20 @@
 public class Solution {
     public static int[] ninjaAndSortedArrays(int arr1[], int arr2[], int m, int n) {
-        int first = 0;
-        int second = 0;
-        int[] result = new int[m+n];
-        int ref = 0;
-        while(first < m && second < n){
-            if(arr1[first] < arr2[second]){
-                result[ref] = arr1[first];
-                ref++;
-                first++;
-            }else{
-                result[ref] = arr2[second];
-                ref++;
-                second++;
+        int first = m-1;
+        int second = n-1;
+        int combined = m+n-1;
+        while(second >= 0){
+            if(first >= 0 && arr1[first] > arr2[second]){
+                arr1[combined] = arr1[first];
+                first--;
+                combined--;
+            }
+            else{
+                arr1[combined] = arr2[second];
+                second--;
+                combined--;
             }
         }
-        while(first < m){
-            result[ref] = arr1[first];
-            ref++;
-            first++;
-        }
-        while(second < n){
-            result[ref] = arr2[second];
-            ref++;
-            second++;
-        }
-        return result;
+        return arr1;
     }
 }
